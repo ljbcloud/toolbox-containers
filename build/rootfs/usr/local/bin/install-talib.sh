5387d7b28d6adf3f882b21de243074feaf22d7d9
ljbcloud <206458089+ljbcloud@users.noreply.github.com>
2025-07-24 20:55:40 +0000

    build: update install-* scripts

--- build/rootfs/usr/local/bin/install-talib.sh
+++ build/rootfs/usr/local/bin/install-talib.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-TA_LIB_VERSION="${TA_LIB_VERSION:-"0.6.4"}"
+TALIB_VERSION="${TALIB_VERSION:-"0.6.4"}"
 
 temp="$(mktemp -d)"
 
-curl -sSL -o "${temp}/ta-lib.tar.gz" "https://github.com/ta-lib/ta-lib/releases/download/v${TA_LIB_VERSION}/ta-lib-${TA_LIB_VERSION}-src.tar.gz"
+curl -sSL -o "${temp}/ta-lib.tar.gz" "https://github.com/ta-lib/ta-lib/releases/download/v${TALIB_VERSION}/ta-lib-${TA_LIB_VERSION}-src.tar.gz"
 
 tar xzvf "${temp}/ta-lib.tar.gz" -C "$temp"
 
@@ -14,5 +14,6 @@ tar xzvf "${temp}/ta-lib.tar.gz" -C "$temp"
 	cd "${temp}/ta-lib-${TA_LIB_VERSION}" || return
 	./configure --prefix=/usr
 	sudo make install
-	rm -rvf "$temp"
 )
+
+rm -rvf "$temp"

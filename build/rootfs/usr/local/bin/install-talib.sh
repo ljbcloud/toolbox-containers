d90aca8b40632cfb9ddfd08ff17425fae6022130
ljbcloud <206458089+ljbcloud@users.noreply.github.com>
2025-07-24 21:45:25 +0000

    tidy: miscellaneous build/ updates (#48)
    * build: add hadolint, pre-commit, uv, and yarn asdf plugins
    
    * build: remove unused phantomjs wrapper
    
    * build: update install-* scripts
    
    * build(fedora): add unrar

--- build/rootfs/usr/local/bin/install-talib.sh
+++ build/rootfs/usr/local/bin/install-talib.sh
@@ -2,17 +2,18 @@
 
 set -e
 
-TA_LIB_VERSION="${TA_LIB_VERSION:-"0.6.4"}"
+TALIB_VERSION="${TALIB_VERSION:-"0.6.4"}"
 
 temp="$(mktemp -d)"
 
-curl -sSL -o "${temp}/ta-lib.tar.gz" "https://github.com/ta-lib/ta-lib/releases/download/v${TA_LIB_VERSION}/ta-lib-${TA_LIB_VERSION}-src.tar.gz"
+curl -sSL -o "${temp}/ta-lib.tar.gz" "https://github.com/ta-lib/ta-lib/releases/download/v${TALIB_VERSION}/ta-lib-${TA_LIB_VERSION}-src.tar.gz"
 
 tar xzvf "${temp}/ta-lib.tar.gz" -C "$temp"
 
 (
-	cd "${temp}/ta-lib-${TA_LIB_VERSION}" || return
+	cd "${temp}/ta-lib-${TALIB_VERSION}" || return
 	./configure --prefix=/usr
 	sudo make install
-	rm -rvf "$temp"
 )
+
+rm -rvf "$temp"
